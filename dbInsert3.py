b556b74b9f46784eed0f086b568fa0271fa0adb9
mj4444 <mohitk856767@gmail.com>
2018-10-09 17:25:10 +0530

    Add files via upload

--- dbInsert3.py
+++ dbInsert3.py
@@ -2,14 +2,22 @@ import dbConnector
 def insertOperations(username,password,role):
 	connection=dbConnector.getConnection()
 	cursor=connection.cursor()
-	query="INSERT INTO USER VALUES(%(username),%(password),%(role))"
-	#query="INSERT INTO USER VALUES(%s,%s,%s)"
-	value=[username,password,role]
+	
+	query="INSERT INTO USER (username,role) VALUES(%s,%s)"
+	#query="INSERT INTO USER VALUES(%s,%s,%s)"	->working
+	
+	value=[username,role]
 	cursor.execute(query,value)
 	connection.commit()
 	
 	print("{0} record Inserted".format(cursor.rowcount))
 	
-insertOperations("Jenny","jenny123","HR")
+insertOperations("Kyle","kyle123","HR")
 
 #not working
+
+
+
+#add_salary = ("INSERT INTO salaries "
+#              "(emp_no, salary, from_date, to_date) "
+#              "VALUES (%(emp_no)s, %(salary)s, %(from_date)s, %(to_date)s)")
